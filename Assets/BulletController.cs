b7663fbfab2f64272be97a5e5037fbccf707c9f5
jpjamipark <jpjamipark@gmail.com>
2019-02-06 22:48:33 -0500

    Added Exploding Projectile + fixed health bar bugs

--- Assets/BulletController.cs
+++ Assets/BulletController.cs
@@ -6,6 +6,14 @@ public class BulletController : MonoBehaviour
 {
     public float velX = 5f;
     public GameObject bulletExplosion;
+    public bool exploding = false;
+    public GameObject Projectile;
+    public int numBullets;
+    public int bulletSpeed;
+
+    private Vector3 start;
+    
+
     float velY = 0f;
     Rigidbody2D rb;
     // Start is called before the first frame update
@@ -13,12 +21,26 @@ public class BulletController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        start = transform.position;
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (exploding && (start - transform.position).magnitude > 3)
+        {
+            for (int i = 0; i < numBullets; ++i)
+            {
+                GameObject bullet = Instantiate(Projectile, transform.position, Quaternion.identity);
+                bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed * 0.5f * Mathf.Cos(i * 2 * Mathf.PI / numBullets), bulletSpeed * 0.5f * Mathf.Sin(i * 2 * Mathf.PI / numBullets));
+            }
+            exploding = false;
 
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collide)
