c6a7b9c0a44db7455cc1007c522aaf35899da1a5
jpjamipark <jpjamipark@gmail.com>
2019-04-07 11:34:02 -0400

    Work in progress to get record to glow

--- Assets/Scripts/MIDIParser.cs
+++ Assets/Scripts/MIDIParser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using MidiPlayerTK;
+using MK.Glow.Legacy;
 //using NAudio.Midi;
 using System.IO;
 using System.Linq;
@@ -13,6 +14,12 @@ public class MIDIParser : MonoBehaviour
     //public MidiFile midi;
     public MidiFilePlayer midiFilePlayer;
     public GameObject enemy;
+
+    //Glow pulse parameter. Starts at start, then goes to peak, then goes back to start
+    public float startScatter = 1;
+    public float peakScatter = 5;
+    public float duration = .1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -52,6 +59,7 @@ public class MIDIParser : MonoBehaviour
                 if (note.Midi == 72)
                 {
                     enemy.GetComponent<LaunchAsteroid>().launchAsteroid(true, transform.position, 20);
+                    StartCoroutine(PulseMkGlow(Camera.main.GetComponent<MKGlow>(), startScatter, peakScatter, duration));
                     numBeats++;
                 }
             }
@@ -61,6 +69,36 @@ public class MIDIParser : MonoBehaviour
                 //new behaviour
             }
         }
+    }
+
+    public static IEnumerator PulseMkGlow(MKGlow glow, float startAlpha, float endAlpha, float duration)
+    {
+        // keep track of when the fading started, when it should finish, and how long it has been running&lt;/p&gt; &lt;p&gt;&a
+        var startTime = Time.time;
+        var endTime = Time.time + duration;
+        var elapsedTime = 0f;
+
+        // set the canvas to the start alpha – this ensures that the canvas is ‘reset’ if you fade it multiple times
+        glow.bloomScattering = startAlpha;
+        // loop repeatedly until the previously calculated end time
+        while (Time.time <= endTime)
+        {
+            elapsedTime = Time.time - startTime; // update the elapsed time
+            var percentage = 1 / (duration / elapsedTime); // calculate how far along the timeline we are
+            if (Time.time <= (endTime / 2)) // if we are fading up 
+            {
+                glow.bloomScattering = startAlpha + (endAlpha - startAlpha) * percentage; // calculate the new alpha
+                Debug.Log(endTime / 2);
+            }
+            else // if we are fading down
+            {
+                glow.bloomScattering = endAlpha - (endAlpha - startAlpha) * percentage; // calculate the new alpha
+            }
+
+            yield return new WaitForEndOfFrame(); // wait for the next frame before continuing the loop
+        }
+        Debug.Log(endTime / 2);
+        glow.bloomScattering = startAlpha; // force the alpha to the end alpha before finishing – this is here to mitigate any rounding errors, e.g. leaving the alpha at 0.01 instead of 0
     }
 
     void PlaySong()
