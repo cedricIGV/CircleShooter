4ab8a6a007d354ded129575307956b732c8a955b
cedricIGV <cedrib@umich.edu>
2019-04-10 22:04:40 -0400

    Background and circle fade

--- Assets/LaunchAsteroid.cs
+++ Assets/LaunchAsteroid.cs
@@ -13,14 +13,37 @@ public class LaunchAsteroid : MonoBehaviour
     float nextFire = 0;
     int numBullets = 10;
     Vector2 bulletPos;
+
+    public bool fade;
+
+    public float minimum = 0.0f;
+    public float maximum = 1f;
+    public float duration = 5.0f;
+    public float startTime;
+    Color oldColor;
+
+    private List<GameObject> bullets = new List<GameObject>();
     void Start()
     {
-
+        startTime = Time.time;
+        fade = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (fade == true)
+        {
+            for (int i = 0; i < bullets.Count; ++i)
+            {
+                if(bullets[i] != null)
+                {
+                    float t = (Time.time - startTime) / duration;
+                    //print(Mathf.SmoothStep(minimum, maximum, t));
+                    bullets[i].GetComponent<SpriteRenderer>().color = new Color(oldColor.r, oldColor.g, oldColor.b, oldColor.a - (Time.time - startTime)/duration);
+                }
+            }
+        }
     }
 
     public void launchAsteroid(bool rotate, Vector3 bulletPos, float angle)
@@ -38,6 +61,8 @@ public class LaunchAsteroid : MonoBehaviour
                 bullet.GetComponent<destructableProjectile>().setVelocity(velocity);
                 bullet.GetComponent<destructableProjectile>().rotate = rotate;
                 bullet.GetComponent<destructableProjectile>().bulletPos = bulletPos;
+                oldColor = bullet.GetComponent<SpriteRenderer>().color;
+                bullets.Add(bullet);
             }
         }
     }
---
+++ Assets/fade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fade : MonoBehaviour
+{
+    public bool startFade;
+    public float startTime;
+    public float duration;
+    Color oldColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startFade = false;
+        oldColor = GetComponent<SpriteRenderer>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startFade == true)
+        {
+            float t = (Time.time - startTime) / duration;
+            //print(Mathf.SmoothStep(minimum, maximum, t));
+            GetComponent<SpriteRenderer>().color = new Color(oldColor.r, oldColor.g, oldColor.b, oldColor.a - (Time.time - startTime) / duration);
+        }
+        if (startFade == false)
+        {
+            GetComponent<SpriteRenderer>().color = oldColor;
+        }
+
+    }
+}
